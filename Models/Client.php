d172af45311f1da89dcd6120160ed48be6bd5ff5
testxxxxxxxxx <pppppmarcin@gmail.com>
2025-11-08 22:28:44 +0100

    created update and delete methods for Client class

--- Models/Client.php
+++ Models/Client.php
@@ -24,7 +24,7 @@ class Client {
     public function create(string $name, string $phoneNumber, string $city, string $street, int $buildingNumber, string $nip, string $email): bool {
         try {
             $db = Database::connect();
-            $sql = "INSERT INTO KONTRACHENT(nazwa, telefon, miejscowosc, ulica, numer_budynku, nip, emial) VALUES(:name, :phoneNumber, :city, :street, :buildingNumber, :nip, :email)";
+            $sql = "INSERT INTO KONTRACHENT(nazwa, telefon, miejscowosc, ulica, numer_budynku, nip, email) VALUES(:name, :phoneNumber, :city, :street, :buildingNumber, :nip, :email)";
             $stmt = $db->prepare($sql);
             $stmt->bindParam(":name", $name, PDO::PARAM_STR);
             $stmt->bindParam(":phoneNumber", $phoneNumber, PDO::PARAM_STR);
@@ -33,6 +33,37 @@ class Client {
             $stmt->bindParam(":buildingNumber", $buildingNumber, PDO::PARAM_INT);
             $stmt->bindParam(":nip", $nip, PDO::PARAM_STR);
             $stmt->bindParam(":email", $email, PDO::PARAM_STR);
+            $stmt->execute();
+            return true;
+        } catch(PDOException $err) {
+            return false;
+        }
+    }
+    public function update(int $id, string $phoneNumber, string $city, string $street, int $buildingNumber, string $nip, string $email): bool {
+        try {
+            $db = Database::connect();
+            $sql = "UPDATE KONTRACHENT SET NAZWA=:name, telefon=:phoneNumber, miejscowosc=:city, ulica=:street, numer_budynku=:buildingNumber, nip=:nip, email=:email WHERE id=:id";
+            $stmt = $db->prepare($sql);
+            $stmt->bindParam(":id", $id, PDO::PARAM_INT);
+            $stmt->bindParam(":name", $name, PDO::PARAM_STR);
+            $stmt->bindParam(":phoneNumber", $phoneNumber, PDO::PARAM_STR);
+            $stmt->bindParam(":city", $city, PDO::PARAM_STR);
+            $stmt->bindParam(":street", $street, PDO::PARAM_STR);
+            $stmt->bindParam(":buildingNumber", $buildingNumber, PDO::PARAM_INT);
+            $stmt->bindParam(":nip", $nip, PDO::PARAM_STR);
+            $stmt->bindParam(":email", $email, PDO::PARAM_STR);
+            $stmt->execute();
+            return true; 
+        } catch(PDOException $err) {
+            return false;
+        }
+    }
+    public function delete(int $id): bool {
+        try {
+            $db = Database::connect();
+            $sql = "DELETE FROM KONTRACHENT WHERE id=:id";
+            $stmt = $db->prepare($sql);
+            $stmt->bindParam(":id", $id, PDO::PARAM_INT);
             $stmt->execute();
             return true;
         } catch(PDOException $err) {
